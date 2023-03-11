e1a524137f0bbc5dd9bf7111a1fd5591f23dcb4d
Vladislav <law.vladislav@gmail.com>
2023-03-11 15:05:14 +0300

    get started

---
+++ BusinessCalculation/BusinessCalculation/LoginScreen/AuthViewController.swift
@@ -0,0 +1,19 @@
+//
+//  AuthViewController.swift
+//  BusinessCalculation
+//
+//  Created by VPDev on 11.03.2023.
+//
+
+import UIKit
+
+class AuthViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
---
+++ BusinessCalculation/BusinessCalculation/LoginScreen/RegisterViewController.swift
@@ -0,0 +1,20 @@
+//
+//  RegisterViewController.swift
+//  BusinessCalculation
+//
+//  Created by VPDev on 11.03.2023.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+
+}
