812d973cf60598a3aff93467c9ccb314e486b5d8
Vladislav <law.vladislav@gmail.com>
2023-03-18 16:14:03 +0300

    Firebase ApiManager

---
+++ BusinessCalculation/BusinessCalculation/Service/Firebase/UserModel.swift
@@ -0,0 +1,15 @@
+//
+//  UserModel.swift
+//  BusinessCalculation
+//
+//  Created by VPDev on 18.03.2023.
+//
+
+import Foundation
+
+struct User {
+    var firstName: String
+    var lastName: String
+    var email: String
+    var password: String
+}
