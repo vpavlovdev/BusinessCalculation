41121e7f9f6b6719c459676d0886e90e95b87f91
Vladislav <law.vladislav@gmail.com>
2023-03-20 15:45:18 +0300

    SignIn with G

--- BusinessCalculation/BusinessCalculation/Service/Firebase/ApiManager.swift
+++ BusinessCalculation/BusinessCalculation/Service/Firebase/ApiManager.swift
@@ -28,19 +28,19 @@ class FirebaseAPIManager {
     
     func registrationNewUser(newUser: User) {
         DispatchQueue.main.async {
-        Auth.auth().createUser(withEmail: newUser.email, password: newUser.password) { (result, error) in
-            guard error == nil  else { print("this error "); return }
-            let db = self.configureFB()
-            db.collection("users").addDocument(
-                data: ["firstName" : newUser.firstName,
-                       "lastName" : newUser.lastName,
-                       "uid" : result!.user.uid])
-            { (error) in
-                guard error == nil else { return } //Change label
+            Auth.auth().createUser(withEmail: newUser.email, password: newUser.password) { (result, error) in
+                guard error == nil  else { print("this error "); return }
+                let db = self.configureFB()
+                db.collection("users").addDocument(
+                    data: ["firstName" : newUser.firstName,
+                           "lastName" : newUser.lastName,
+                           "uid" : result!.user.uid])
+                { (error) in
+                    guard error == nil else { return } //Change label
+                }
+                print("Registration compleate")
             }
-            print("Registration compleate")
         }
-    }
     }
     
     func signInUser(email: String, password: String) {
@@ -50,8 +50,26 @@ class FirebaseAPIManager {
         }
     }
     func signInGoogle() {
-        
-        
+        DispatchQueue.main.async {
+            guard let clientID = FirebaseApp.app()?.options.clientID else { return }
+            let config = GIDConfiguration(clientID: clientID)
+            GIDSignIn.sharedInstance.configuration = config
+            
+            guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+                  let window = windowScene.windows.first,
+                  let rootViewController = window
+                    .rootViewController else { print("There is no root ViewController"); return }
+            GIDSignIn.sharedInstance.signIn(withPresenting: rootViewController) { result, error in
+                guard error == nil else { return }
+                guard let user = result?.user,
+                      let idToken = user.idToken?.tokenString else { return }
+                let credential = GoogleAuthProvider.credential(withIDToken: idToken, accessToken: user.accessToken.tokenString)
+                Auth.auth().signIn(with: credential) { result, error in
+                    
+                }
+            }
+
+        }
     }
 }
 
