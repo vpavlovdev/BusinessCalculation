c7ac27b58c24cfc5b3040885f00a44b53040ef4a
Vladislav <law.vladislav@gmail.com>
2023-03-29 21:29:00 +0300

    editProfile

--- BusinessCalculation/BusinessCalculation/Modules/MainScreen/EditProfileViewController.swift
+++ BusinessCalculation/BusinessCalculation/Modules/MainScreen/EditProfileViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 final class EditProfileViewController: UIViewController {
-
+    private lazy var deleteProfileButton = createBarButtonItem(imageName: ImageNames.deleteButton, action: #selector(deleteButtonTapped))
+    //MARK: Life cicle
+   
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .mainScreen
+        setupUserInterface()
+        setupNavigationBar()
         
     }
- 
-
+    //MARK: Setup NavBar
+    private func setupNavigationBar() {
+        navigationItem.rightBarButtonItem = deleteProfileButton
+        navigationItem.backButtonTitle = ""
+    }
+    //MARK: Setup UserInterface
+    private func setupUserInterface() {
+        view.backgroundColor = .mainScreen
+    }
+    //MARK: Private methods
+    @objc private func deleteButtonTapped() {
+        print(#function)
+    }
+}
+extension EditProfileViewController: CreateNavBarItemProtocol {
+    //Create custom button for NavBar
 }
