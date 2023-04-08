a598bb91cbc61ff8a3d71dc09952b818dcc79c40
Vladislav <law.vladislav@gmail.com>
2023-04-08 10:19:25 +0300

    userInfo

--- BusinessCalculation/BusinessCalculation/Modules/MainScreen/EditProfileScreen/EditProfileViewController.swift
+++ BusinessCalculation/BusinessCalculation/Modules/MainScreen/EditProfileScreen/EditProfileViewController.swift
@@ -6,11 +6,45 @@
 //
 
 import UIKit
+fileprivate enum LocalConstants {
+    static let widthProfileImage: CGFloat = 150
+    static let heightProfileImage: CGFloat = 150
+    static let cornerRadius: CGFloat = heightProfileImage / 2
+}
 
 final class EditProfileViewController: UIViewController {
     private lazy var deleteProfileButton = createBarButtonItem(imageName: ImageNames.deleteButton, action: #selector(deleteButtonTapped))
-    //MARK: Life cicle
     
+    private let profileImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: ImageNames.userImage)
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.layer.cornerRadius = LocalConstants.cornerRadius
+        imageView.layer.zPosition = 1
+        return imageView
+    }()
+    private let shadowView: UIView = {
+        let view = UIView()
+        view.clipsToBounds = false
+        view.backgroundColor = .mainScreen
+        view.layer.cornerRadius = LocalConstants.cornerRadius
+        view.layer.shadowColor = UIColor.selectedTabBarItem.cgColor
+        view.layer.shadowOffset = CGSize(width: 2.0, height: 8.0)
+        view.layer.shadowRadius = 15.0
+        view.layer.shadowOpacity = 0.75
+        view.layer.shouldRasterize = true
+        view.layer.rasterizationScale = UIScreen.main.scale
+        view.layer.zPosition = 0.1
+        return view
+    }()
+    private let nameLabel: UILabel = {
+        let label = UILabel()
+        return label
+    }()
+    
+    
+    //MARK: Life cicle
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUserInterface()
@@ -25,6 +59,21 @@ final class EditProfileViewController: UIViewController {
     //MARK: Setup UserInterface
     private func setupUserInterface() {
         view.backgroundColor = .mainScreen
+        [profileImageView, shadowView].forEach {
+            view.addSubview($0)
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
+        NSLayoutConstraint.activate([
+            profileImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 30),
+            profileImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            profileImageView.widthAnchor.constraint(equalToConstant: LocalConstants.widthProfileImage),
+            profileImageView.heightAnchor.constraint(equalToConstant: LocalConstants.heightProfileImage),
+            
+            shadowView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 30),
+            shadowView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            shadowView.widthAnchor.constraint(equalToConstant: LocalConstants.widthProfileImage),
+            shadowView.heightAnchor.constraint(equalToConstant: LocalConstants.heightProfileImage),
+    ])
     }
     //MARK: Private methods
     @objc private func deleteButtonTapped() {
@@ -58,8 +107,8 @@ final class EditProfileViewController: UIViewController {
     private func createInfoOfDeleteAlert(error: Bool) {
         if error {
             let alert = UIAlertController(title: "Внимание",
-                                           message: "Произошла ошибка, повторите попытку позже",
-                                           preferredStyle: .alert)
+                                          message: "Произошла ошибка, повторите попытку позже",
+                                          preferredStyle: .alert)
             let action = UIAlertAction(title: "Спасибо", style: .cancel) { action in
                 
             }
